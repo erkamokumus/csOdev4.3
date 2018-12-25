0e32180eacec793eb334215ce9dddcb60a23b103
muhammederkam85 <44413751+muhammederkam85@users.noreply.github.com>
2018-12-25 16:46:29 +0300

    Add files via upload

---
+++ cs3.java
@@ -0,0 +1,31 @@
+package arrays;
+
+import java.util.Scanner;
+
+public class cs3 {
+	public static void main(String[] args)
+	{
+		//Write a function that tests whether a string is a palindrome
+		//3. ödev
+		
+		
+		System.out.println("Lutfen bir kelime girin:");
+		Scanner scan = new Scanner(System.in);
+		String word = scan.nextLine();
+		
+		System.out.println(palindrome(word));
+	}
+	
+	public static boolean palindrome(String wordf)
+	{
+		for(int i=0; i<wordf.length();i++)
+		{
+			if(wordf.charAt(i) == wordf.charAt( (wordf.length() )-1-i ))
+			{
+				return true;
+			}
+		}
+		return false;
+	}
+
+}
